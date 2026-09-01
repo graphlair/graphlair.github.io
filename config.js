e81e247256ccc1473fe170621824f62e9c2cef2e
graphlair <nargis1p1@gmail.com>
2026-09-01 23:46:58 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-export const SUPABASE_URL = 'https://kfkywtkgueprlcjarntv.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_M8qlxBjA0UNYxc9fzhX_rA_yYNEJTZ6';
+export const SUPABASE_URL = 'https://smzkgwhvtaykmwrgfusk.supabase.co';
+
+export const SUPABASE_ANON_KEY = 'sb_publishable_Y3TXgrm1xf9QlI2s2Dqlpw_7mNNdMa-';
